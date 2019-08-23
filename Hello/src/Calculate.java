b61504706f0472c5ebe771627e4c9620e4f58412
adnane <ouadi.mcrp@gmail.com>
2019-08-23 15:03:54 +0200

    modification de systeme

--- Hello/src/Calculate.java
+++ Hello/src/Calculate.java
@@ -4,11 +4,12 @@ public class Calculate {
 	public static void main(String[] args) {
 
 		
-		String names []= {"ao","ad","mo","in"};
+		String names []= {"ao","ad","mo","in","adnane"};
 		
 		for (String fox : names) {
 		
 			System.out.println(fox);
+			System.out.println("noooo");
 		}
 	}
 
