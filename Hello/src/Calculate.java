e9e263450dd066c229d0ee8efa8261df0603ebff
adnane <ouadi.mcrp@gmail.com>
2019-08-23 14:23:37 +0200

    Second Commit

---
+++ Hello/src/Calculate.java
@@ -0,0 +1,15 @@
+
+public class Calculate {
+
+	public static void main(String[] args) {
+
+		
+		String names []= {"ao","ad","mo","in"};
+		
+		for (String fox : names) {
+		
+			System.out.println(fox);
+		}
+	}
+
+}
